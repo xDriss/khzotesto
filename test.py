0462c7af835d8efc463202299180b1f959439a0a
xDriss <123965804+xDriss@users.noreply.github.com>
2023-02-25 13:13:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+from selenium import webdriver
 
+# Replace PATH_TO_CHROMEDRIVER with the path to your ChromeDriver executable
+driver = webdriver.Chrome(PATH_TO_CHROMEDRIVER)
+
+# Replace URL with the URL of the webpage you want to open
+driver.get("https://yip.su/Winners")
+
+# Print the title of the webpage
+print(driver.title)
+
+# Close the browser window
+driver.quit()
